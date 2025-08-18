0567485fb6e97a2f5b281792e2891333bdcda6aa
EthanMik <etmik@icloud.com>
2025-08-17 19:27:47 -0500

    Working Sensors

--- js/script.js
+++ js/script.js
@@ -15,6 +15,8 @@ canvas.height = canvasHeight_px
 const fieldWidth_in = 1.5
 const fieldHeight_in = 1.5
 
+const distance_max_range = 78;
+
 const kPX = 96;
 
 function to_px(inches) { return inches / ((canvasWidth_px / scale) / (fieldWidth_in * scale)) * kPX; }
@@ -29,27 +31,97 @@ function clamp(value, min, max) { return Math.max(min, Math.min(max, value)); }
 const reset_face = { FRONT: 0, LEFT: 1, RIGHT: 2, REAR: 3 }; 
 const wall = { TOP: 70, LEFT: -70, RIGHT: 70, BOTTOM: -70 };
 
+const kFrontOffset = 0;
+const kRightOffset = 90;
+const kLeftOffset = 270;
+const kRearOffset = 180;
+
+function get_offset(index) {
+    switch (index) {
+        case 0:
+            return kFrontOffset;
+        case 1:
+            return kLeftOffset;
+        case 2:
+            return kRightOffset;
+        case 3:
+            return kRearOffset;
+        default:
+            return 0;
+    }    
+}
+
+function get_name(enum_name, value) {
+    for (const key in enum_name) {
+        if (enum_name[key] === value) {
+            return key.toLowerCase();
+        }
+    }
+    return '';
+}
+
 class distance {
-    constructor(reset_face, x_offset, y_offset) {
+    constructor(reset_face, x_offset, y_offset, render = true) {
         this.reset_face = reset_face;
         this.x_offset = x_offset;
         this.y_offset = y_offset;
+        this.render = render;
     }
 
     get_reset_axis(reset_face, wall, angle) {
 
     }
+
+    get_distance(x, y, angle, offset) {
+        return this.ray_cast(x, y, angle, offset).distance;
+    }
+
+    ray_cast(robot_x, robot_y, robot_angle, offset) {
+        const angle = to_inertial_rad(robot_angle + offset);
+        const global_angle = to_inertial_rad(robot_angle);
+        const dx = Math.cos(angle);
+        const dy = -Math.sin(angle);
+
+        const offset_x = this.x_offset * Math.cos(global_angle) + this.y_offset * -Math.sin(global_angle);
+        const offset_y = this.x_offset * -Math.sin(global_angle) - this.y_offset * Math.cos(global_angle);
+
+        const x = robot_x + offset_x;
+        const y = robot_y + offset_y;
+
+        let tMax = Infinity;
+
+        if (dx > 0) tMax = Math.min(tMax, (wall.RIGHT - x) / dx);
+        if (dx < 0) tMax = Math.min(tMax, (wall.LEFT - x) / dx);
+        if (dy > 0) tMax = Math.min(tMax, (wall.TOP - y) / dy);
+        if (dy < 0) tMax = Math.min(tMax, (wall.BOTTOM - y) / dy);
+
+        var start_x = x;
+        var start_y = y;
+        var end_x = x + dx * tMax;
+        var end_y = y + dy * tMax;
+        var distance = Math.sqrt((end_x - x) ** 2 + (end_y - y) ** 2);
+
+        if (distance > distance_max_range) {
+            end_x = x;
+            end_y = y;
+            distance = NaN;
+        }
+
+        return { start_x, start_y, end_x, end_y, distance };
+    }
 }
 
 
 class Robot {
-    constructor(width, height, odomData = false, color = '#969696ff') {
+    constructor(width, height, reset_sensors, odomData = false, color = '#969696ff') {
         this.x_ = 0;
         this.y_ = 0;
         this.width_ = width;
         this.height_ = height;
         this.angle_ = 0;
         this.odomData_ = odomData;
+        this.reset_sensors = {};
+        this.reset_sensors = reset_sensors
         this.color_ = color;
     }
 
@@ -61,7 +133,7 @@ class Robot {
     get_y() { return this.y_ }
     get_angle() { return this.angle_; }
 
-    odom_data() {
+    draw_odom_data() {
         if (!this.odomData_) { return; }
         ctx.save();
         ctx.font = '20px Calibri';
@@ -73,52 +145,47 @@ class Robot {
         ctx.restore();
     }
 
-    ray_cast(x_offset = 0, y_offset = 0) {
-        const angleRad = to_rad(this.angle_);
-        const dx = Math.cos(angleRad);
-        const dy = -Math.sin(angleRad);
-
-        const x = this.x_ + x_offset;
-        const y = this.y_ - y_offset;
-
-        let tMax = Infinity;
-
-        if (dx > 0) tMax = Math.min(tMax, (wall.RIGHT - x) / dx);
-        if (dx < 0) tMax = Math.min(tMax, (wall.LEFT - x) / dx);
-        if (dy > 0) tMax = Math.min(tMax, (wall.TOP - y) / dy);
-        if (dy < 0) tMax = Math.min(tMax, (wall.BOTTOM - y) / dy);
-
-        const end_x = x + dx * tMax;
-        const end_y = y + dy * tMax;
-        const distance = Math.sqrt((end_x - x) ** 2 + (end_y - y) ** 2);
-
-        return { end_x, end_y, distance };
-    }
-
-    reset_sensors() {
+    draw_reset_sensors() {
         ctx.save();
 
-        const ray = this.ray_cast(0, 0);
-        ctx.beginPath();
-        ctx.moveTo(to_pxx(this.x_), to_pxy(this.y_));
-        ctx.lineTo(to_pxx(ray.end_x), to_pxy(ray.end_y));
-        ctx.strokeStyle = '#2fff74ff';
-        ctx.lineWidth = 2;
-        ctx.stroke();
+        let text_height = 95;
+        for (const distance of Object.values(reset_face)) {
+            if (this.reset_sensors[distance].render == false) { continue; }
+            let offset = get_offset(distance);
+            const ray = this.reset_sensors[distance].ray_cast(this.x_, this.y_, this.angle_, offset);
 
-        ctx.font = '20px Calibri';
-        ctx.fillStyle = 'white';
-        ctx.textBaseline = 'top';
-        ctx.fillText(`Distance: ${ray.distance.toFixed(2)}`, 20, 95);
+            ctx.beginPath();
+            ctx.moveTo(to_pxx(ray.start_x), to_pxy(ray.start_y));
+            ctx.lineTo(to_pxx(ray.end_x), to_pxy(ray.end_y));
+    
+            const rectWidth = 10;
+            const rectHeight = 5;
+            ctx.save();
+            ctx.translate(to_pxx(ray.start_x), to_pxy(ray.start_y));
+            ctx.rotate(to_inertial_rad(this.angle_ + offset));
+            ctx.fillStyle = '#1b1717ff';
+            ctx.fillRect(-rectWidth/2, -rectHeight/2, rectWidth, rectHeight);
+            ctx.restore();
+    
+            ctx.strokeStyle = '#ff4d40ff';
+            ctx.lineWidth = 2;
+            ctx.stroke();
+    
+            ctx.font = '20px Calibri';
+            ctx.fillStyle = 'white';
+            ctx.textBaseline = 'top';
+            ctx.fillText(`${get_name(reset_face, distance)}: ${this.reset_sensors[distance].get_distance(this.x_, this.y_, this.angle_, offset).toFixed(2)}`, 20, text_height);
+            text_height += 25;
 
-        ctx.restore();
+            ctx.restore();
+        }
     }
 
-    chassis() {
+    draw_chassis() {
         ctx.save();
 
         ctx.translate(to_pxx(this.x_), to_pxy(this.y_));
-        ctx.rotate(to_rad(this.angle_));
+        ctx.rotate(to_inertial_rad(this.angle_));
         ctx.fillStyle = this.color_;
         ctx.fillRect(-to_px(this.width_) / 2, -to_px(this.height_) / 2, to_px(this.width_), to_px(this.height_));
 
@@ -133,9 +200,9 @@ class Robot {
     }
 
     render() {
-        this.odom_data();
-        this.chassis();
-        this.reset_sensors();
+        this.draw_odom_data();
+        this.draw_chassis();
+        this.draw_reset_sensors();
     }
 }
 
@@ -147,14 +214,16 @@ function loadImage(src) {
 
 let fieldPerimeter = loadImage("./assets/field_perimeter.png");
 
-let front_reset = new distance(reset_face.FRONT, 0, 0);
-let left_reset = new distance(reset_face.LEFT, 0, 0);
-let right_reset = new distance(reset_face.RIGHT, 0, 0);
-let rear_reset = new distance(reset_face.REAR, 0, 0);
+let reset_sensors = {};
+reset_sensors[reset_face.FRONT] = new distance(reset_face.FRONT, 5, 3.5, true);
+reset_sensors[reset_face.LEFT] = new distance(reset_face.LEFT, -6, -4, true);
+reset_sensors[reset_face.RIGHT] = new distance(reset_face.RIGHT, 0, 5, false);
+reset_sensors[reset_face.REAR] = new distance(reset_face.REAR, -5, 4, false);
 
 let robot = new Robot(
     14, // Width
     14, // Height
+    reset_sensors,
     true // Enable telemetry
 );
 
